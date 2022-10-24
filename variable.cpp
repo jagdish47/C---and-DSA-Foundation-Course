1ac8dfc317aeea843cb7ff1da663f71207aca26c
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-24 21:20:23 +0530

    variables and datatypes

---
+++ variable.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // this is single line comment
+    /* this is multiple line comment
+    where you can comment multiple line compiler won't run this code*/
+
+    int apples = 4;
+    char grade = 'A';
+    float per = 98.55;
+    bool flag1 = true;
+    bool flag2 = false;
+    double pie = 3.14333334343;
+    wchar_t wide_character = L'x';
+    // wide characater give you ascii value of that character wchar_t and name = (L) is fixed '' than give character 
+    
+     cout<<apples<<endl;
+    cout<<grade<<endl;
+    cout<<per<<endl;
+    cout<<flag1<<endl;
+    cout<<pie<<endl;
+    cout<<wide_character<<endl;
+
+    // endl use to point compiler to next line
+
+    
+    return 0;
+}
