0032afcca240f7d94101df6ad0219e12933e72a2
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-24 21:30:02 +0530

    Taking input from user

---
+++ input.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // we are taking input as number
+    int a;
+    cout<<"enter a : ";
+    cin>>a;
+
+    // we are taking input as character
+    char ch;
+    cout<<"enter char : ";
+    cin>>ch;
+
+    //taking input as float
+    float per;
+    cout<<"enter per : ";
+    cin>>per;
+
+
+    // show output 
+    cout<<a<<endl;
+    cout<<ch<<endl;
+    cout<<per<<endl;
+
+    return 0;
+}
