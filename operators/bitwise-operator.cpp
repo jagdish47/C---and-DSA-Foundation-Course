3c0b8b6a2f0431adf1696806776c8018bf8229c5
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-25 22:09:42 +0530

    bitwise-operator

---
+++ operators/bitwise-operator.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // int num1 = 5;
+    // cout<<(num1 << 1)<<endl;
+    // cout<<(num1 << 2)<<endl;
+    // cout<<(num1 << 3)<<endl;
+
+    // int num2 = 10;
+    // cout<<(num2 >> 1)<<endl;
+    // cout<<(num2 >> 2)<<endl;
+    // cout<<(num2 >> 3)<<endl;
+    // cout<<(num2 >> 4)<<endl;
+    // cout<<(num2 >> 5)<<endl;
+
+
+    // int num3 = 100;
+    // cout<<(~num3)<<endl;
+ 
+    int num4 = 4;
+    int num5 = 8;
+
+    cout<<(num4 | num5)<<endl;
+    cout<<(num4 & num5)<<endl;
+
+ return 0;
+}
