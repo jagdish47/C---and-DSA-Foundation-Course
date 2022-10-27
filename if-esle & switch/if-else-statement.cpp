0d84f45bdb7670c4c2dd998b5e338879d7eeaa9e
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-27 09:56:20 +0530

    if-else statement

---
+++ if-esle & switch/if-else-statement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int marks;
+    cout<<"enter marks : ";
+    cin>>marks;
+
+    if(marks > 33){
+        cout<<"Passed"<<endl;
+    }
+    else{
+        cout<<"Failed"<<endl;
+    }
+}
