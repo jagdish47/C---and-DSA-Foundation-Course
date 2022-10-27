cb3889f8a7a7f7431a693b12639d20d41936c0e0
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-27 11:16:24 +0530

    concept of bitwise and and logical and

---
+++ if-esle & switch/logical-bitwise-and.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x = 3;
+    int y = 7;
+    int z = 10;
+    int j = 0;
+
+    // if(z > x && z > y){
+    //     cout<<"both true"<<endl;
+    // }
+
+    // int a =( x & y );
+    // cout<<a<<endl;
+
+
+
+    // cout<<( x && y)<<endl; //non zero values are always true and 0 is always false
+    // cout<<(j && x) <<endl;
+
+
+
+    // float m = 1.0;
+    // float n = 2.0;
+
+    // cout<< ( m & n) <<endl;  //we cannot use float value in bitwise and it will give you compiler error
+
+
+    
+
+    return 0;
+}
