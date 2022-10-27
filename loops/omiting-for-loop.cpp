34fa699151fa073581e9695ffe447ef53458c888
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-27 23:57:45 +0530

    playing with for loop

---
+++ loops/omiting-for-loop.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // int i = 0;
+    // for(;i < 10; i++){
+    //     cout<<i<<endl;
+    // }
+
+
+    // infinite loop
+    //give random memory address
+    // for(int i = 0; ; i++){
+    //     cout<<i<<endl;
+    // }
+
+
+    //infine loop
+    //give only 0's
+    // for(int i = 0; i < 10; ){
+    //     cout<<i<<endl;
+    // }
+
+
+    // INFINE LOOP`
+    // int i = 10;
+    // for(;;){
+    //     cout<<i<<endl;
+    // }
+
+    //multiple conditions
+    // for(int i = 1, j = 10; i < 10, j > 0; i++, j-- ){
+    //     cout<<i << "  "<<j<<endl;
+    // }
+
+    return 0;
+}
