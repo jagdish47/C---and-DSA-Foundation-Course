6e4bf03eb9559489597e0f78a80afd66dbae920a
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-27 23:34:24 +0530

    while loop and first n natural number

---
+++ loops/while-loop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    cout<<"enter number : ";
+    cin>>num;
+
+
+    int i = 1;
+
+    while(i <= num){
+        cout<<i<<" ";
+        i++;
+    }
+
+    return 0;
+}
