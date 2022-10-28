5b7b205f578f2d67af54a7ae0387c4157e078d79
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-28 12:38:00 +0530

    diagonal line pattern

---
+++ pattern-printing-part1/pattern-3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+    int m, n;
+    cout<<"enter m and n : ";
+    cin>>m>>n;
+
+    for(int i = 1; i <=m; i++ ){
+
+        for(int j = 1; j <= n; j++){
+
+            if(j == i){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+// *         
+//   *       
+//     *     
+//       *   
+//         * 
