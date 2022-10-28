4476d039e03ff453cc4d55e4d1b4f91b46663c12
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-28 17:37:20 +0530

    rectangular number pattern

---
+++ pattern-printing-part1/pattern-10.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int m;
+    cout<<"enter m : ";
+    cin>>m;
+
+
+    for(int i = 1; i <= m; i++){
+
+        for(int j = 1; j <= m; j++){
+
+            cout<<j <<" ";
+        }
+
+        cout<<endl;
+    }
+
+
+}
+
+
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
+// 1 2 3 4 5 
