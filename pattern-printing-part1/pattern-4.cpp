914dc0cfdddfea903ccc2d930928446860644c76
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-28 12:46:04 +0530

    z pattern

---
+++ pattern-printing-part1/pattern-4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int m, n;
+    cout<<"enter m and n : ";
+    cin>>m>>n;
+
+
+    for(int i = 1; i <= m; i++){
+
+        for(int j = n; j > 0; j--){
+
+            if(i == 1 || i == m || i == j){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
