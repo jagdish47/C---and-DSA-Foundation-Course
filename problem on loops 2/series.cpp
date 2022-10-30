c5b4bd15117af6d4c0cf86ef2205b3697dec093e
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-30 21:58:10 +0530

    series of digit

---
+++ problem on loops 2/series.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+    int n;
+    cout<<"enter n : ";
+    cin>>n;
+
+    int sum = 0;
+
+    for(int i = 1; i <= n; i++){
+        if(i % 2 == 0){
+            sum += i;
+        }
+        else{
+            sum -= i;
+        }
+    }
+
+    cout<<sum<<endl;
+
+    return 0;
+}
