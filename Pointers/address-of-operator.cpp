2efa2cff429d83aab004933969cad89affa65230
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-12-08 09:25:29 +0530

    address of operator &

---
+++ Pointers/address-of-operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    cout << "actual value : " << x << endl;
+    cout << "address of x : " << &x << endl;
+
+    return 0;
+}
+
+// & = is a address of operator which gives you address of particular block in memory(RAM)
