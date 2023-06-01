acc4f38771ee71060e6dd59372f5d4538f7b7e3a
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2023-06-01 19:17:27 +0530

    first repeating

---
+++ PRACTISE ONLY C++/first-repeating-element.cpp
@@ -0,0 +1,25 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+
+//     vector<int> arr{1, 5, 3, 4, 3, 5, 6};
+
+//     int ans = -1;
+
+//     unordered_map<int, int> hash;
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         hash[arr[i]]++;
+//     }
+
+//     for (int i = 0; hash.size(); i++)
+//     {
+
+//         cout << hash.
+//     }
+
+//     return 0;
+// }
