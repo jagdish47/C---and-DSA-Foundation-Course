66babc47e545476d2bac4af1571c1bda56509926
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2023-07-01 13:31:52 +0530

    heap

---
+++ Heaps/Lecture-2/priority-queue-max-heap.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    priority_queue<int> pq;
+
+    pq.push(4);
+    pq.push(7);
+    pq.push(3);
+    pq.push(10);
+    pq.push(9);
+    pq.push(1);
+
+    cout << pq.top() << endl;
+    pq.pop();
+    cout << pq.top() << endl;
+    pq.pop();
+    cout << pq.top() << endl;
+    pq.pop();
+    cout << pq.top() << endl;
+    pq.pop();
+    cout << pq.top() << endl;
+    pq.pop();
+    cout << pq.top() << endl;
+    pq.pop();
+
+    if (pq.empty())
+    {
+        cout << "Yes, it's empty" << endl;
+    }
+    else
+    {
+        cout << "No, it's not empty" << endl;
+    }
+
+    return 0;
+}
